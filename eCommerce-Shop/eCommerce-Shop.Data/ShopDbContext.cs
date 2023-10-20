6f97b378bae7a773c920ede6c5946d49de457832
JacksonJS12 <d.ivan0v.6100@gmail.com>
2023-10-21 01:22:13 +0300

    Initial.

---
+++ eCommerce-Shop/eCommerce-Shop.Data/ShopDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace eCommerce_Shop.Data
+{
+    public class ShopDbContext : IdentityDbContext
+    {
+        public ShopDbContext(DbContextOptions<ShopDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
